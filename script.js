a4f7cf37d88ebc86e1d2e4dc96dc901d507577c1
AK-ishaan <236677156+AK-ishaan@users.noreply.github.com>
2025-11-04 18:13:28 +0530

    On dynamically adding tickets Deletion and changing priority feature is added

--- script.js
+++ script.js
@@ -52,20 +52,58 @@ saveBtn.addEventListener('click',function(){
 
 
 //ticket creation
-const main=document.querySelector('main')
+const main = document.querySelector('main');
 
-function ticketCreation(textcont,key,selectedColor){
-  let ticketbox=document.createElement("div");
-  ticketbox.setAttribute("class","ticket");
-  ticketbox.innerHTML=`<div class="ticket-priority-color" style="background-color:${selectedColor}"></div>
-          <div class="ticket-id">ID : ${key}</div>
-          <div class="ticket-area">${textcont}</div>
-          <div class="ticket-delete">
-              <i class="fa-solid fa-trash"></i>
-          </div>`
-    main.appendChild(ticketbox);
-    priorityLevel.forEach(act=>{
-        act.classList.remove("active")
-    });
+
+function ticketCreation(textcont, key, selectedColor) {
+  let ticketbox = document.createElement("div");
+  ticketbox.setAttribute("class", "ticket");
+  ticketbox.innerHTML = `
+    <div class="ticket-priority-color" style="background-color:${selectedColor}"></div>
+    <div class="ticket-id">${key}</div>
+    <div class="ticket-area">${textcont}</div>
+    <div class="ticket-delete">
+        <i class="fa-solid fa-trash dltBtn"></i>
+    </div>
+  `;
+  
+  main.appendChild(ticketbox);
+  
+  //adding changing priority colour feature
+  changePriority(ticketbox)
+
+  // adding ticket deletion feature
+  dltTask(ticketbox);
+  
+ 
+
+  // clear active classes
+  priorityLevel.forEach(act => {
+    act.classList.remove("active");
+  });
+  alert("* Double click on delete icon to delete your task\n* Double click on task top colour to change your priority");
+
 }
 
+
+function dltTask(taskBlock) {
+  let dlt = taskBlock.querySelector('.dltBtn');
+  dlt.addEventListener('dblclick', function() {
+    
+    taskBlock.remove();
+  });
+}
+
+//feature function for changing task priority
+function changePriority(taskcont) {
+  const colors = ['green', 'yellow', 'red', 'black'];
+  const priorityCont = taskcont.querySelector('.ticket-priority-color');
+
+  let currentColorIndex = colors.indexOf(priorityCont.style.backgroundColor);
+  if (currentColorIndex === -1) currentColorIndex = 0;
+
+  priorityCont.addEventListener('dblclick', function() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    priorityCont.style.backgroundColor = colors[currentColorIndex];
+  });
+}
